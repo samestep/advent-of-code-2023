3e1b554e43307c953882e626a25be528e03925bd
Sam Estep <sam@samestep.com>
2023-12-19 16:42:01 -0500

    Set `base` in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { join, resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/advent-of-code-2023/",
   build: {
     rollupOptions: {
       input: ["", "day01"].map((dir) =>
